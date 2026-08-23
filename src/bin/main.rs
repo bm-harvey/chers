c1c85af8d1d5ab575e8cede1b2fb05cbfbc138aa
Bryan M Harvey <bmharvey@live.com>
2026-08-23 14:58:53 -0500

    updated to display debug info

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -5,11 +5,22 @@ use chers::game_state::RenderType;
 
 fn main() -> Result<()> {
     let mut game = Game::new();
-    let game_viewer = GameViewer::new().with_render_type(RenderType::ASCII);
-    game_viewer.print(&game);
+    let game_viewer = GameViewer::new().with_render_type(RenderType::Normal);
+
     game.natural_apply_move("e2e4")?;
     game.natural_apply_move("d7d5")?;
+    game.natural_apply_move("e4d5")?;
+    game.natural_apply_move("d8d5")?;
+    game.natural_apply_move("a2a4")?;
+    game.natural_apply_move("d5d3")?;
+    game.natural_apply_move("a1a3")?;
+    game.natural_apply_move("d3d6")?;
+    game.natural_apply_move("a3d4")?;
+    game.natural_apply_move("d6d8")?;
     game_viewer.print(&game);
+
+    game_viewer.print_square_idxs();
+    game_viewer.print_legal_moves(&game, 27);
 
     Ok(())
 }
