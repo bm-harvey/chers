473b9544e258875f1cd138f9ce77e4fa381d8f84
Bryan M Harvey <bmharvey@live.com>
2026-08-25 18:43:45 -0500

    meaningless shuffle of test code

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,11 +1,13 @@
 use anyhow::Result;
-//use chers::view::{GameViewer, RenderType};
-use chers::chess::Game;
+use chers::chess::{BoardState, Game};
+use chers::core::bits;
+use chers::view;
+use chers::view::{GameViewer, RenderType};
 use rand::prelude::*;
 
 fn main() -> Result<()> {
     //let mut game = Game::new();
-    //let game_viewer = GameViewer::new().with_render_type(RenderType::Normal);
+    let game_viewer = GameViewer::new().with_render_type(RenderType::Normal);
 
     //game.natural_apply_move("e2e4")?;
     //game.natural_apply_move("d7d5")?;
@@ -19,9 +21,20 @@ fn main() -> Result<()> {
     //game.natural_apply_move("d6d8")?;
     //game_viewer.print(&game);
 
-    //game_viewer.print_square_idxs();
+    game_viewer.print_square_idxs();
+    //
+
     //game_viewer.print_legal_moves(&game, 27);
 
+    //bits::dbg_mask(0x03_03_03_03_03_03_03_03);
+    //bits::dbg_mask(0xc0_c0_c0_c0_c0_c0_c0_c0);
+    //bits::dbg_mask(0x00_00_00_00_00_00_ff_ff);
+    //bits::dbg_mask(0xff_ff_00_00_00_00_00_00);
+
+    Ok(())
+}
+
+fn run_time_test() -> Result<()> {
     let mut total_eval = 0_f32;
     let num_iters = 1_000_000;
 
@@ -38,12 +51,11 @@ fn main() -> Result<()> {
 
             game.apply_move_by_coords(file_1, rank_1, file_2, rank_2, None)?;
 
-            total_eval += game.board_state().count_material();
+            total_eval += game.board_state().material_value();
         }
     }
 
     println!("Avg. Eval : {}", total_eval / num_iters as f32);
     println!("Time : {}", start.elapsed().as_secs_f32());
-
     Ok(())
 }
